78a183dfb6792fcbea94723919a75296fca9623f
Giorgio Comai (OBCT/CCI) <comai@balcanicaucaso.org>
2018-06-05 13:45:33 +0000

    adding trending hashtag EN

---
+++ QuoteFinderBot/script_botTwitterMEP_trending_EN.R
@@ -0,0 +1,8 @@
+if (!require("pacman")) install.packages("pacman", repos = "https://cloud.r-project.org") # for taking care of package installation/loading
+pacman::p_load("knitr", repos = "https://cloud.r-project.org")
+
+setwd(file.path("QuoteFinderScripts", "QuoteFinderBot"))
+
+rmarkdown::render(input = "botTwitterMEP_hashtags_EN.Rmd",
+                  output_file = paste0(Sys.time(), "-botTwitterMEP_trending_EN.html"),
+                  output_dir = "logs")
