ce60ceeb9c161dd65c22492913022fcca07408c6
Giorgio Comai (OBCT/CCI) <comai@balcanicaucaso.org>
2018-06-12 14:17:37 +0000

    additional scheduled bot posts

--- QuoteFinderBot/script_botTwitterMEP_3monthsStarTotal_it.R
+++ QuoteFinderBot/script_botTwitterMEP_3monthsStarTotal_it.R
@@ -1,8 +1,9 @@
 if (!require("pacman")) install.packages("pacman", repos = "https://cloud.r-project.org") # for taking care of package installation/loading
-pacman::p_load("knitr", repos = "https://cloud.r-project.org")
+pacman::p_load("knitr")
+#pacman::p_load("ezknitr")
 
 setwd(file.path("QuoteFinderScripts", "QuoteFinderBot"))
 
-rmarkdown::render(input = "botTwitterMEP_trending_EN.Rmd",
-                  output_file = paste0(Sys.time(), "-botTwitterMEP_trending_EN.html"),
+rmarkdown::render(input = "botTwitterMEP_3monthsStarTotal_it.Rmd",
+                  output_file = paste0(Sys.time(), "-botTwitterMEP_3monthsStarTotal_it.html"),
                   output_dir = "logs")
--- QuoteFinderBot/script_botTwitterMEP_trending_IT.R
+++ QuoteFinderBot/script_botTwitterMEP_trending_IT.R
@@ -1,8 +1,8 @@
 if (!require("pacman")) install.packages("pacman", repos = "https://cloud.r-project.org") # for taking care of package installation/loading
-pacman::p_load("knitr", repos = "https://cloud.r-project.org")
+pacman::p_load("knitr")
 
 setwd(file.path("QuoteFinderScripts", "QuoteFinderBot"))
 
-rmarkdown::render(input = "botTwitterMEP_trending_EN.Rmd",
-                  output_file = paste0(Sys.time(), "-botTwitterMEP_trending_EN.html"),
+rmarkdown::render(input = "botTwitterMEP_trending_IT.Rmd",
+                  output_file = paste0(Sys.time(), "-botTwitterMEP_trending_IT.html"),
                   output_dir = "logs")
